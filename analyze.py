84d30baf7d3c3cc62799b41a49fc76123a9ade6f
Dmitry Detkov <maliciousgenius@gmail.com>
2021-04-24 03:13:48 +0300

    added seeds & fix for analyze

--- analyze.py
+++ analyze.py
@@ -12,13 +12,24 @@ def  create_dataframe(name, text_files_dir):
     data = []
 
     for text_file in os.scandir(name + "/" + text_files_dir):
-        if text_file.is_file() and text_file.path.split('.')[-1].lower() == 'summary':
-            for line in open(text_file, 'r+').readlines():
-                if line.find('—') > 0 and line[0].isupper():
-                    data.append([text_file.name.split('.')[0], uuid.uuid4(), line])
-    
+        if text_file.is_file() and text_file.path.split('.')[-1].lower() == "summary":
+            for line in open(text_file, "r+").readlines():
+                if line.find('—') > 0 and line[-1] != ';' and line[0].isupper():
+                    if line[-1] == "\n":
+                        data.append([text_file.name.split('.')[0], str(uuid.uuid4()), "\"" + line[:-1] + "\""])
+                    else:
+                        data.append([text_file.name.split('.')[0], str(uuid.uuid4()), "\"" + line + "\""])
+
+    for text_file in os.scandir(name + "/" + text_files_dir):
+        if text_file.is_file() and text_file.path.split('.')[-1].lower() == "text":
+            for line in open(text_file, "r+").readlines():
+                if line.find('—') > 0 and line[-1] != ';' and line[0].isupper():
+                    if line[-1] == "\n":
+                        data.append([text_file.name.split('.')[0], str(uuid.uuid4()), "\"" + line[:-1] + "\""])
+                    else:
+                        data.append([text_file.name.split('.')[0], str(uuid.uuid4()), "\"" + line + "\""])
     
     with open(name + "/" + name + ".csv", 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file, delimiter=";")
         for item in data:
-            csv_writer.writerow([item])
+            csv_writer.writerow(item)
